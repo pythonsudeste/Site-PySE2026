c654275ea7770071008b5826211317c78ef5d214
Thiago Medeiros <thiagotitico1@gmail.com>
2026-01-08 17:52:48 -0300

    Arrumando o basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Site-Python-Sudeste-2026">
+    <BrowserRouter basename="/Site-PySE2026">
       <App />
     </BrowserRouter>
   </StrictMode>,
