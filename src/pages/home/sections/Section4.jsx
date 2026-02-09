909062183f589fae1680a5fa42509b2cd8d2ce63
Thiago Medeiros <thiagotitico1@gmail.com>
2026-02-09 18:29:37 -0300

    Subindo alteração para ativar a submissão de palestras

--- src/pages/home/sections/Section4.jsx
+++ src/pages/home/sections/Section4.jsx
@@ -8,9 +8,14 @@ export default function Section4() {
             <div className='Section4-itens'>
                 <img src={img} alt="Logo da python sudeste" />
                 <div className='Section4-itens-texto'>
-                    <h4>Confira a Agenda Completa!</h4>
-                    <p>Veja todos os detalhes da programação da Python Sudeste 2026 e não perca nenhuma atividade.</p>
-                    <Link to="/"><button>Acessar Agenda</button></Link>
+                    {/* <h4>Confira a Agenda Completa!</h4> */}
+                    <h4>Faça parte da Python Sudeste 2026!</h4>
+                    {/* <p>Veja todos os detalhes da programação da Python Sudeste 2026 e não perca nenhuma atividade.</p> */}
+                    <p>Submeta agora sua palestra ou tutorial, e contribua com a nossa comunidade.</p>
+                    {/* <Link to="/"><button>Acessar Agenda</button></Link> */}
+                    <a href="https://talks.python.org.br/pysul2026/cfp" target='_blank'>
+                        <button>Clique aqui e envie</button>
+                    </a>
                 </div>
             </div>
         </div>
