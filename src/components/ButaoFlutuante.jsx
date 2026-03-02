9deb4426429a7b099029bd745132ce4fcd8f822a
Thiago Medeiros <thiagotitico1@gmail.com>
2026-03-02 12:07:33 -0300

    Adicionando o botão flutuante para melhor navegação principalmente para mobile alterando o banner do mobile, colocando a data correta

--- src/components/ButaoFlutuante.jsx
+++ src/components/ButaoFlutuante.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './styles/ButaoFlutuante.css'
 
 export default function ButaoFlutuante() {
@@ -6,36 +7,17 @@ export default function ButaoFlutuante() {
     const openModal = () => setIsModalOpen(true);
     const closeModal = () => setIsModalOpen(false);
 
-    const [isSelecionarLinguaOpen, setIsSelecionarLinguaOpen] = useState(false);
-    const openSelecionarLingua = () => setIsSelecionarLinguaOpen(true);
-    const closeSelecionarLingua = () => setIsSelecionarLinguaOpen(false);
-
     return (<>
         <div className={`ButaoFlutuante ${isModalOpen ? "invisivel" : ""}`} onClick={openModal}>
-            <h4>Menu</h4>
+            Menu
         </div>
 
         <div className={`Modal ${!isModalOpen ? "invisivel" : ""}`}>
-            <div className={`Modal_opçoes ${isSelecionarLinguaOpen ? "invisivel" : ""}`}>
-                <h6 className={`Modal_opçoes_linguagem`}
-                    onClick={openSelecionarLingua}>
-                    Selecionar Lingua
-                </h6>
-                <a href=""><h6>Ingressos</h6></a>
-                <a href=""><h6>Local</h6></a>
-                <a href=""><h6>Palestrantes</h6></a>
-                <a href=""><h6>Sobre</h6></a>
-            </div>
-
-            <div className={`Modal_opçoes_linguagem ${!isSelecionarLinguaOpen ? "invisivel" : ""}`}>
-                <h6>Portugues</h6>
-                <h6>Ingles</h6>
-                <h6>Espanhol</h6>
-
-                <h5 onClick={closeSelecionarLingua}>
-                    sair
-                </h5>
-            </div>
+            <a href="https://2026.sudeste.python.org.br/">Inicio</a>
+            <a href="https://2026.sudeste.python.org.br/ingressos">Ingressos aqui!</a>
+            <a href="https://2026.sudeste.python.org.br/submissao">Submeta sua Talk</a>
+            <a href="https://2026.sudeste.python.org.br/sobre">Sobre o Evento</a>
+            <a href="https://2026.sudeste.python.org.br/local">Local</a>
             <button onClick={closeModal}>Fechar</button>
         </div>
     </>)
