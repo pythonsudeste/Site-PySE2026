43d73ab71eda56048365f310a977c156007a9a61
Thiago Medeiros <thiagotitico1@gmail.com>
2026-04-22 22:32:12 -0300

    Subindo alterações de loadind para melhorar a otimização do site

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,57 @@
 import { Routes, Route } from "react-router-dom"
-import Layout from "./Layout"
-import Sobre from "./pages/sobre/Sobre"
-import Home from "./pages/home/Home"
-import PaginaPalestrantes from "./pages/palestrantes/PaginaPalestrantes"
-import Local from "./pages/local/Local"
-import Carregamento from "./components/Carregamento"
-import Ingressos from "./pages/ingressos/Ingressos"
+import { Suspense } from "react";
 import { useEffect, useState } from "react"
-import Submissao from "./pages/sumissao/Submissao"
+import Layout from "./Layout"
+import PaginaPalestrantes from "./pages/palestrantes/PaginaPalestrantes"
+import Carregamento from "./components/Carregamento"
 import TicketsModal from "./components/TicketsModal"
+import React from "react"
+
+const Local = React.lazy(() => import("./pages/local/Local"));
+const Sobre = React.lazy(() => import("./pages/sobre/Sobre"));
+const Ingressos = React.lazy(() => import("./pages/ingressos/Ingressos"));
+const Submissao = React.lazy(() => import("./pages/sumissao/Submissao"));
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  const [ Home , setHome] = useState(null);
+  const [ loaded , setloaded ] = useState(false);
 
   useEffect(() => {
+    import("./pages/home/Home").then((mod) => {
+      setHome(() => mod.default);
+    });
+
+    // setloaded(true)
+
     setTimeout(() => {
-      setLoading(false);
+      setloaded(true)
     }, 2000);
+
+    import("./pages/local/Local");
+    import("./pages/sobre/Sobre");
+    import("./pages/ingressos/Ingressos");
+    import("./pages/sumissao/Submissao");
   }, []);
 
+  if (!Home || !loaded) {
+    return <Carregamento />
+  }
+
   return (<>
-      {loading && <Carregamento />}
       <TicketsModal/>
       
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          {/* <Route path="paginapalestrantes" element={<PaginaPalestrantes />} /> */}
-          <Route path="local" element={<Local />} />
-          <Route path="sobre" element={<Sobre />} />
-          <Route path="ingressos" element={<Ingressos />} />
-          <Route path="submissao" element={<Submissao />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Carregamento />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            {/* <Route path="paginapalestrantes" element={<PaginaPalestrantes />} /> */}
+            <Route path="local" element={<Local />} />
+            <Route path="sobre" element={<Sobre />} />
+            <Route path="ingressos" element={<Ingressos />} />
+            <Route path="submissao" element={<Submissao />} />
+          </Route>
+        </Routes>
+      </Suspense>
   </>);
 }
 
