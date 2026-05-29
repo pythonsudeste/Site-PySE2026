0b42f55bd06bd1f07912d51ec14427b5f4599f79
Thiago Medeiros <thiagotitico1@gmail.com>
2026-05-29 18:20:34 -0300

    removendo o botao de submissão de talk

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,6 @@ export default function Header() {
                 <Link to={linksTicketSaleSite} target="_blank"><button>Ingressos aqui!</button></Link>
             </div>
             <div className="Header-rotas">
-                <Link to="/submissao"><h4>Submeta sua Talk</h4></Link>
                 <Link to="/sobre"><h4>Sobre o Evento</h4></Link>
                 <Link to="/local"><h4>Local</h4></Link>
                 {/* <Link to="/paginapalestrantes"><h4>Programação</h4></Link> */}
