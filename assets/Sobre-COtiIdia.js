7f2befd80a36c5cf43205f4b136b13bcb4cdafb6
Thiago Medeiros <thiagotitico1@gmail.com>
2026-06-29 17:53:01 -0300

    Updates

--- assets/Sobre-COtiIdia.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BGe7yOaG.js";import{S as t}from"./Section2-Cs6lsSDR.js";function e(){return r.jsxs(r.Fragment,{children:[r.jsx("div",{style:{marginTop:"40px"}}),r.jsx(t,{})]})}export{e as default};
---
+++ assets/Sobre-TREC6RMz.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DYVwoCAM.js";import{S as t}from"./Section2-Chk-r-7-.js";function e(){return r.jsxs(r.Fragment,{children:[r.jsx("div",{style:{marginTop:"40px"}}),r.jsx(t,{})]})}export{e as default};
